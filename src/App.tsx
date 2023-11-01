da4460eafd669d1ad48c7b5a31ad6b587938cadd
gajindubandara <gajindukb@gmail.com>
2023-11-01 11:48:33 +0530

    Update to hashrouting

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Route, Routes, Link, } from "react-router-dom";
+import { HashRouter as Router, Route, Routes, Link, } from "react-router-dom";
 import Login from "./scense/Login";
 import Dashboard from "./scense/Dashboard";
 import Profile from "./scense/Profile";
