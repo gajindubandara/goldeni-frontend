f4242c7abb6accca8a68694ea7d340317322ec4c
gajindubandara <gajindukb@gmail.com>
2023-11-06 16:59:19 +0530

    Removes unused imports

--- src/scenes/Login.tsx
+++ src/scenes/Login.tsx
@@ -1,12 +1,12 @@
 import { Button } from "antd";
-import { useGoogleLogin } from "@react-oauth/google";
-import { useEffect, useState } from "react";
-import axios from "axios";
-import { useNavigate } from 'react-router-dom';
+// import { useGoogleLogin } from "@react-oauth/google";
+// import { useEffect } from "react";
+// import axios from "axios";
+// import { useNavigate } from 'react-router-dom';
 
 const Login: React.FC = () => {
-  const navigate = useNavigate();
-  const [user, setUser] = useState<any | null>(null);
+  // const navigate = useNavigate();
+  // const [user, setUser] = useState<any | null>(null);
   // const login = useGoogleLogin({
   //   onSuccess: (codeResponse) => setUser(codeResponse),
   //   onError: (error) => console.log("Login Failed:", error),
@@ -18,34 +18,34 @@ window.location.href = 'https://goldeni.auth.ap-southeast-1.amazoncognito.com/lo
 
   }
 
-  useEffect(() => {
-    if (user) {
-      axios
-        .get(
-          `https://www.googleapis.com/oauth2/v1/userinfo?access_token=${user.access_token}`,
-          {
-            headers: {
-              Authorization: `Bearer ${user.access_token}`,
-              Accept: "application/json",
-            },
-          }
-        )
-        .then((res) => {
-          // setProfile(res.data);
-          localStorage.setItem("userData", JSON.stringify(res.data));
-          localStorage.setItem("loggedIn", "true");
-          const expirationTime = (new Date().getTime() + 3600000).toString(); // 1 hour in milliseconds
-          localStorage.setItem("sessionExpiration", expirationTime);
-          // console.log(res);
-          // window.location.href = "/index.html#/";
-          navigate('/');
-          window.location.reload();
-          // navigate('/dashboard');
-          // window.location.href = window.location.href.split('#')[0] + '#/dashboard';
-        })
-        .catch((err) => console.log(err));
-    }
-  }, [navigate, user]);
+  // useEffect(() => {
+  //   if (user) {
+  //     axios
+  //       .get(
+  //         `https://www.googleapis.com/oauth2/v1/userinfo?access_token=${user.access_token}`,
+  //         {
+  //           headers: {
+  //             Authorization: `Bearer ${user.access_token}`,
+  //             Accept: "application/json",
+  //           },
+  //         }
+  //       )
+  //       .then((res) => {
+  //         // setProfile(res.data);
+  //         localStorage.setItem("userData", JSON.stringify(res.data));
+  //         localStorage.setItem("loggedIn", "true");
+  //         const expirationTime = (new Date().getTime() + 3600000).toString(); // 1 hour in milliseconds
+  //         localStorage.setItem("sessionExpiration", expirationTime);
+  //         // console.log(res);
+  //         // window.location.href = "/index.html#/";
+  //         navigate('/');
+  //         window.location.reload();
+  //         // navigate('/dashboard');
+  //         // window.location.href = window.location.href.split('#')[0] + '#/dashboard';
+  //       })
+  //       .catch((err) => console.log(err));
+  //   }
+  // }, [navigate, user]);
 
   const src =
     "https://res.cloudinary.com/dwldehfnr/video/upload/v1698683188/goldeni-frontend/xy9lis5g5rkd11q3azfh.mp4";
