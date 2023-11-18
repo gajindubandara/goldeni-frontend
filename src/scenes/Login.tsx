82b829da8e592cbd3d40953e0e470bd1bc4df829
gajindubandara <gajindukb@gmail.com>
2023-11-18 22:46:48 +0530

    Implement common variables

--- src/scenes/Login.tsx
+++ src/scenes/Login.tsx
@@ -1,11 +1,11 @@
 import {Button} from "antd";
 import React from "react";
+import { loginUrl } from '../services/commonVariables';
 
 const Login: React.FC = () => {
 
     const login = () => {
-        window.location.href = 'https://goldeni.auth.ap-southeast-1.amazoncognito.com/login?client_id=2ch8tjlfbqpa6nbf19ak670lrs&response_type=token&scope=email+openid+profile&redirect_uri=https%3A%2F%2Fgoldeni.s3.ap-southeast-1.amazonaws.com%2Findex.html';
-        // window.location.href = 'https://goldeni.auth.ap-southeast-1.amazoncognito.com/login?client_id=2ch8tjlfbqpa6nbf19ak670lrs&response_type=token&scope=email+openid+profile&redirect_uri=http%3A%2F%2Flocalhost%3A3000';
+        window.location.href = loginUrl;
 
     }
     const src =
