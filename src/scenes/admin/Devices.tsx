a661fcca73889c127837844d307047121a102b43
gajindubandara <gajindukb@gmail.com>
2024-02-04 02:01:06 +0530

    Integrate devices admin api

--- src/scenes/admin/Devices.tsx
+++ src/scenes/admin/Devices.tsx
@@ -1,103 +1,142 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import AppLayout from "../../layout/AppLayout";
 import { Button, Input, Table, Tag, Space } from "antd";
-
 import { SearchOutlined } from '@ant-design/icons';
+import { baseUrl } from '../../services/commonVariables';
 
 const Devices: React.FC = () => {
+    const [data, setData] = useState([]);
+    const [loading, setLoading] = useState(true);
+    const idToken = localStorage.getItem("idToken");
+
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const response = await fetch(`${baseUrl}/admin/devices`, {
+                    headers: {
+                        Authorization: `Bearer ${idToken}`                    }
+                });
+                const jsonData = await response.json();
+                setData(jsonData);
+                setLoading(false);
+            } catch (error) {
+                console.error("Error fetching data:", error);
+                setLoading(false);
+            }
+        };
+
+        fetchData();
+    }, []);
 
     const handleSearch = (selectedKeys: React.Key[], confirm: () => void, dataIndex: string) => {
-      confirm();
+        confirm();
     };
 
     const handleReset = (clearFilters: () => void) => {
-      clearFilters();
+        clearFilters();
     };
 
     const getColumnSearchProps = (dataIndex: string) => ({
-      filterDropdown: ({ setSelectedKeys, selectedKeys, confirm, clearFilters }: any) => (
-        <div style={{ padding: 8 }}>
-          <Input
-            placeholder={`Search ${dataIndex}`}
-            value={selectedKeys[0]}
-            onChange={(e) => setSelectedKeys(e.target.value ? [e.target.value] : [])}
-            onPressEnter={() => handleSearch(selectedKeys, confirm, dataIndex)}
-            style={{ width: 188, marginBottom: 8, display: 'block' }}
-          />
-          <Space>
-            <Button
-              type="primary"
-              onClick={() => handleSearch(selectedKeys, confirm, dataIndex)}
-            //   icon={<SearchOutlined />}
-              size="small"
-              style={{ width: 90 }}
-            >
-              Search
-            </Button>
-            <Button onClick={() => handleReset(clearFilters)} size="small" style={{ width: 90 }}>
-              Reset
-            </Button>
-          </Space>
-        </div>
-      ),
-      filterIcon: (filtered: boolean) => (
-        <SearchOutlined style={{ color: filtered ? '#1890ff' : undefined }} />
-      ),
-      onFilter: (value: any, record: any) =>
-        record[dataIndex].toString().toLowerCase().includes(value.toLowerCase()),
+        filterDropdown: ({ setSelectedKeys, selectedKeys, confirm, clearFilters }: any) => (
+            <div style={{ padding: 8 }}>
+                <Input
+                    placeholder={`Search ${dataIndex}`}
+                    value={selectedKeys[0]}
+                    onChange={(e) => setSelectedKeys(e.target.value ? [e.target.value] : [])}
+                    onPressEnter={() => handleSearch(selectedKeys, confirm, dataIndex)}
+                    style={{ width: 188, marginBottom: 8, display: 'block' }}
+                />
+                <Space>
+                    <Button
+                        type="primary"
+                        onClick={() => handleSearch(selectedKeys, confirm, dataIndex)}
+                        size="small"
+                        style={{ width: 90 }}
+                    >
+                        Search
+                    </Button>
+                    <Button onClick={() => handleReset(clearFilters)} size="small" style={{ width: 90 }}>
+                        Reset
+                    </Button>
+                </Space>
+            </div>
+        ),
+        filterIcon: (filtered: boolean) => (
+            <SearchOutlined style={{ color: filtered ? '#1890ff' : undefined }} />
+        ),
+        onFilter: (value: any, record: any) =>
+            record[dataIndex].toString().toLowerCase().includes(value.toLowerCase()),
     });
 
-    const dataSource = [
-        { key: '1', name: 'John Doe', age: 25, address: '123 Main St',tags: ['nice', 'developer'], },
-        { key: '2', name: 'Jane Doe', age: 30, address: '456 Oak St',tags: ['nice', 'developer'], },
-        { key: '3', name: 'John Doe', age: 25, address: '123 Main St' ,tags: ['nice', 'developer'],},
-        { key: '4', name: 'Jane Doe', age: 30, address: '456 Oak St' ,tags: ['nice', 'developer'],},
-        { key: '5', name: 'John Doe', age: 25, address: '123 Main St',tags: ['nice', 'developer'], },
-        { key: '6', name: 'Jane Doe', age: 30, address: '456 Oak St' ,tags: ['nice', 'developer'],},
-        { key: '7', name: 'John Doe', age: 25, address: '123 Main St',tags: ['nice', 'developer'], },
-        { key: '8', name: 'Jane Doe', age: 30, address: '456 Oak St' ,tags: ['nice', 'developer'],},
-
-        // Add more data as needed
-    ];
-
     const columns = [
-        { title: 'Name', dataIndex: 'name', key: 'name', ...getColumnSearchProps('name') },
-        { title: 'Age', dataIndex: 'age', key: 'age', ...getColumnSearchProps('age') },
-        { title: 'Address', dataIndex: 'address', key: 'address', ...getColumnSearchProps('address') },
+        { title: 'Device ID', dataIndex: 'deviceId', key: 'deviceId', ...getColumnSearchProps('deviceId') },
         {
-            title: 'Tags',
-            key: 'tags',
-            dataIndex: 'tags',
-            render: (_: any, { tags }: { tags: string[] }) => (
-              <>
-                {tags.map((tag) => {
-                  let color = tag.length > 5 ? 'geekblue' : 'green';
-                  if (tag === 'loser') {
-                    color = 'volcano';
-                  }
-                  return (
-                    <Tag color={color} key={tag}>
-                      {tag.toUpperCase()}
-                    </Tag>
-                  );
-                })}
-              </>
+            title: 'Registered Email',
+            dataIndex: 'registeredEmail',
+            key: 'registeredEmail',
+            ...getColumnSearchProps('registeredEmail'),
+            render: (registeredEmail: string) => (registeredEmail ? registeredEmail : '-'),
+        },
+        {
+            title: 'Emergency Contact Numbers',
+            dataIndex: 'emergencyContactNumbers',
+            key: 'emergencyContactNumbers',
+            render: (emergencyContactNumbers: string[]) => (
+                <>
+                    {emergencyContactNumbers.map((number, index) => (
+                        <div key={index}>{number}</div>
+                    ))}
+                </>
             ),
-          },
-        // Add more columns as needed
-      ];
+        },
+        { title: 'Mac Address', dataIndex: 'macAddress', key: 'macAddress' },
+        {
+            title: 'Enrollment Status',
+            key: 'status',
+            dataIndex: 'status',
+            render: (status: string) => {
+                let color = status === 'ENROLLED' ? 'geekblue' : status === 'NEW' ? 'green' : 'volcano';
+                return (
+                    <Tag color={color} key={status}>
+                        {status.toUpperCase()}
+                    </Tag>
+                );
+            },
+        },
+        {
+            title: 'Connectivity',
+            key: 'connection',
+            dataIndex: 'connected',
+            render: (connection: boolean) =>  {
+                let value = connection ? 'Connected' : 'Disconnected'
+                let color = connection ? 'green' : 'red';
+                return (
+                    <Tag color={color} key={value}>
+                        {value.toUpperCase()}
+                    </Tag>
+                );
+            },
+        },
+    ];
 
     const paginationConfig = {
         pageSize: 5, // Display 5 items per page
-      };
-  return (
-    <AppLayout>
-        <div className="section-break">
-      <h1>Devices</h1>
-      <Table dataSource={dataSource} columns={columns} pagination={paginationConfig} />
-      </div>
-    </AppLayout>
-  );
+    };
+
+    return (
+        <AppLayout>
+            <div className="section-break">
+                <h1>Devices</h1>
+                <Table
+                    dataSource={data}
+                    columns={columns}
+                    pagination={paginationConfig}
+                    loading={loading}
+                    rowKey={(record) => record.id}
+                />
+            </div>
+        </AppLayout>
+    );
 };
 
 export default Devices;
--- src/services/commonVariables.js
+++ src/services/commonVariables.js
@@ -2,6 +2,4 @@
 export const loginUrl = 'https://goldeni.auth.ap-southeast-1.amazoncognito.com/login?client_id=2ch8tjlfbqpa6nbf19ak670lrs&response_type=token&scope=email+openid+profile&redirect_uri=https%3A%2F%2Fgoldeni.s3.ap-southeast-1.amazonaws.com%2Findex.html';
 //localhost login url
 // export const loginUrl = 'https://goldeni.auth.ap-southeast-1.amazoncognito.com/login?client_id=2ch8tjlfbqpa6nbf19ak670lrs&response_type=token&scope=email+openid+profile&redirect_uri=http%3A%2F%2Flocalhost%3A3000';
-
-
-export const apiUrl = 'https://api.example.com';
+export const baseUrl = 'http://192.168.1.11:9191';
