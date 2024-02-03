372828a42b1135e7c86873790492c235bfc3f17f
gajindubandara <gajindukb@gmail.com>
2024-02-04 03:20:14 +0530

    Update backend base url

--- src/services/commonVariables.js
+++ src/services/commonVariables.js
@@ -2,4 +2,5 @@
 export const loginUrl = 'https://goldeni.auth.ap-southeast-1.amazoncognito.com/login?client_id=2ch8tjlfbqpa6nbf19ak670lrs&response_type=token&scope=email+openid+profile&redirect_uri=https%3A%2F%2Fgoldeni.s3.ap-southeast-1.amazonaws.com%2Findex.html';
 //localhost login url
 // export const loginUrl = 'https://goldeni.auth.ap-southeast-1.amazoncognito.com/login?client_id=2ch8tjlfbqpa6nbf19ak670lrs&response_type=token&scope=email+openid+profile&redirect_uri=http%3A%2F%2Flocalhost%3A3000';
-export const baseUrl = 'http://192.168.1.11:9191';
+// export const baseUrl = 'http://192.168.1.11:9191';
+export const baseUrl = 'https://demo-g54s.onrender.com';
