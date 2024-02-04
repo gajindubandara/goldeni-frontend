757aaf501678f2542e796085cd58c25df5b59535
gajindubandara <gajindukb@gmail.com>
2024-02-04 22:43:48 +0530

    Integrate admin view users api

--- src/scenes/admin/Users.tsx
+++ src/scenes/admin/Users.tsx
@@ -1,103 +1,68 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import AppLayout from "../../layout/AppLayout";
-import { Button, Input, Table, Tag, Space } from "antd";
-
-import { SearchOutlined } from '@ant-design/icons';
+import { Table, Tag, Avatar } from "antd";
+import {avatarPlaceHolder, baseUrl} from '../../services/commonVariables';
+import axios from 'axios';
 
 const Users: React.FC = () => {
+    const [data, setData] = useState([]);
+    const [loading, setLoading] = useState(true);
+    const idToken = localStorage.getItem("idToken");
 
-    const handleSearch = (selectedKeys: React.Key[], confirm: () => void, dataIndex: string) => {
-      confirm();
-    };
 
-    const handleReset = (clearFilters: () => void) => {
-      clearFilters();
-    };
+    useEffect(() => {
+        const fetchData = () => {
+            axios.get(`${baseUrl}/admin/users`, {
+                headers: {
+                    Authorization: `Bearer ${idToken}`
+                }
+            })
+                .then(response => {
+                    setData(response.data);
+                    setLoading(false);
+                })
+                .catch(error => {
+                    console.error("Error fetching data:", error);
+                    setLoading(false);
+                });
+        };
 
-    const getColumnSearchProps = (dataIndex: string) => ({
-      filterDropdown: ({ setSelectedKeys, selectedKeys, confirm, clearFilters }: any) => (
-        <div style={{ padding: 8 }}>
-          <Input
-            placeholder={`Search ${dataIndex}`}
-            value={selectedKeys[0]}
-            onChange={(e) => setSelectedKeys(e.target.value ? [e.target.value] : [])}
-            onPressEnter={() => handleSearch(selectedKeys, confirm, dataIndex)}
-            style={{ width: 188, marginBottom: 8, display: 'block' }}
-          />
-          <Space>
-            <Button
-              type="primary"
-              onClick={() => handleSearch(selectedKeys, confirm, dataIndex)}
-            //   icon={<SearchOutlined />}
-              size="small"
-              style={{ width: 90 }}
-            >
-              Search
-            </Button>
-            <Button onClick={() => handleReset(clearFilters)} size="small" style={{ width: 90 }}>
-              Reset
-            </Button>
-          </Space>
-        </div>
-      ),
-      filterIcon: (filtered: boolean) => (
-        <SearchOutlined style={{ color: filtered ? '#1890ff' : undefined }} />
-      ),
-      onFilter: (value: any, record: any) =>
-        record[dataIndex].toString().toLowerCase().includes(value.toLowerCase()),
-    });
-
-    const dataSource = [
-        { key: '1', name: 'John Doe', age: 25, address: '123 Main St',tags: ['nice', 'developer'], },
-        { key: '2', name: 'Jane Doe', age: 30, address: '456 Oak St',tags: ['nice', 'developer'], },
-        { key: '3', name: 'John Doe', age: 25, address: '123 Main St' ,tags: ['nice', 'developer'],},
-        { key: '4', name: 'Jane Doe', age: 30, address: '456 Oak St' ,tags: ['nice', 'developer'],},
-        { key: '5', name: 'John Doe', age: 25, address: '123 Main St',tags: ['nice', 'developer'], },
-        { key: '6', name: 'Jane Doe', age: 30, address: '456 Oak St' ,tags: ['nice', 'developer'],},
-        { key: '7', name: 'John Doe', age: 25, address: '123 Main St',tags: ['nice', 'developer'], },
-        { key: '8', name: 'Jane Doe', age: 30, address: '456 Oak St' ,tags: ['nice', 'developer'],},
-
-        // Add more data as needed
-    ];
+        fetchData();
+    }, []);
 
     const columns = [
-        { title: 'Name', dataIndex: 'name', key: 'name', ...getColumnSearchProps('name') },
-        { title: 'Age', dataIndex: 'age', key: 'age', ...getColumnSearchProps('age') },
-        { title: 'Address', dataIndex: 'address', key: 'address', ...getColumnSearchProps('address') },
         {
-            title: 'Tags',
-            key: 'tags',
-            dataIndex: 'tags',
-            render: (_: any, { tags }: { tags: string[] }) => (
-              <>
-                {tags.map((tag) => {
-                  let color = tag.length > 5 ? 'geekblue' : 'green';
-                  if (tag === 'loser') {
-                    color = 'volcano';
-                  }
-                  return (
-                    <Tag color={color} key={tag}>
-                      {tag.toUpperCase()}
-                    </Tag>
-                  );
-                })}
-              </>
-            ),
-          },
-        // Add more columns as needed
-      ];
+            title: 'Avatar',
+            dataIndex: 'picture',
+            key: 'picture',
+            render: (picture: string) => (
+                picture !== 'none' ? <Avatar src={picture} /> : <Avatar src={avatarPlaceHolder} />
+            )
+        },
+        {
+            title: 'Name',
+            dataIndex: 'name',
+            key: 'name'
+        },
+        {
+            title: 'Email',
+            dataIndex: 'email',
+            key: 'email'
+        }
+    ];
 
     const paginationConfig = {
-        pageSize: 5, // Display 5 items per page
-      };
-  return (
-    <AppLayout>
-        <div className="section-break">
-      <h1>Users</h1>
-      <Table dataSource={dataSource} columns={columns} pagination={paginationConfig} />
-      </div>
-    </AppLayout>
-  );
+        pageSize: 5
+    };
+
+    return (
+        <AppLayout>
+            <div className="section-break">
+                <h1>Users</h1>
+                <Table dataSource={data} columns={columns} pagination={paginationConfig} loading={loading} />
+            </div>
+        </AppLayout>
+    );
 };
 
 export default Users;
